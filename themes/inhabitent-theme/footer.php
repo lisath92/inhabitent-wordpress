272ed1762f0f60f479419de07e05ca0a9aa6eb59
lisath92 <lisa.hsu.419@gmail.com>
2016-08-14 20:12:27 -0700

    re-indent scss js and footer php

--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -1,32 +1,42 @@
-<?php
+<?php 
 /**
- * The template for displaying the footer.
- *
- * @package RED_Starter_Theme
- */
-
-?>
-
-</div><!-- #content -->
+/* The template for displaying the footer.
+/* @package inhabitent_theme
+**/ ?>
+</div>
+<!-- #content -->
 <footer id="colophon" class="site-footer" role="contentinfo">
   <div class="wrapper">
     <div class="site-info">
-      <h1>Contact Info</h1>
-      <p><i class="fa fa-envelope" aria-hidden="true"></i> info@inhabitent.com</p>
-      <p><i class="fa fa-phone" aria-hidden="true"></i> 778-456-7891</p>
-      <i class="fa fa-facebook-square" aria-hidden="true"></i>
-      <i class="fa fa-twitter-square" aria-hidden="true"></i>
-      <i class="fa fa-google-plus-square" aria-hidden="true"></i>
-    </div><!--site-info-->
-    <a href="<?php echo home_url() ?>"><img src="<?php echo get_template_directory_uri().'/assets/images/inhabitent-logo-text.svg'; ?>" alt="Full logo linking back to home page"/></a>
+      <h1>Contact Info
+      </h1>
+      <p>
+        <i class="fa fa-envelope" aria-hidden="true">
+        </i> info@inhabitent.com
+      </p>
+      <p>
+        <i class="fa fa-phone" aria-hidden="true">
+        </i> 778-456-7891
+      </p>
+      <i class="fa fa-facebook-square" aria-hidden="true">
+      </i>
+      <i class="fa fa-twitter-square" aria-hidden="true">
+      </i>
+      <i class="fa fa-google-plus-square" aria-hidden="true">
+      </i>
+    </div>
+    <!--site-info-->
+    <a href="<?php echo home_url() ?>">
+      <img src="<?php echo get_template_directory_uri().'/assets/images/inhabitent-logo-text.svg'; ?>" alt="Full logo linking back to home page"/>
+    </a>
   </div>
+  <!-- wrapper -->
   <p class="copyright">Copyright &copy; 2016 Inhabitent
-  </p><!-- .site-info -->
-</div><!-- wrapper -->
-</footer><!-- #colophon -->
-</div><!-- #page -->
-
+  </p>
+</footer>
+<!-- #colophon -->
+</div>
+<!-- #page -->
 <?php wp_footer(); ?>
-
 </body>
 </html>
--- themes/inhabitent-theme/js/main.js
+++ themes/inhabitent-theme/js/main.js
@@ -1,24 +1,21 @@
-$(function(){
-  
-//Opens search bar once search icon has been clicked
-  $('body').on('click', function(event){
-    console.log(event.target);
-    var searchBar = $('input[type="search"]');
+$(function() {
 
-    if ($(event.target).parents().hasClass("search-form")){
-      event.preventDefault();
-      searchBar.show('slow');
-      searchBar.focus(); //check if button was clicked
+    //Opens search bar once search icon has been clicked
+    $('body').on('click', function(event) {
+        console.log(event.target);
+        var searchBar = $('input[type="search"]');
 
-    } else if (searchBar.val().length >0){
-      // event.preventDefault();
-      searchBar.focusout();
-      searchBar.show(); //check if there is entered search string
-    } else {
-      searchBar.hide('slow');
-      searchBar.focusout(); //if no search string, close search bar
-    }
-  }); //End search bar open function
+        if ($(event.target).parents().hasClass("search-form")) {
+            event.preventDefault();
+            searchBar.show('slow');
+            searchBar.focus(); //check if button was clicked
 
-
+        } else if (searchBar.val().length > 0) {//end first if statement
+            searchBar.focusout();
+            searchBar.show(); //check if there is entered search string
+        } else { //end second if statement
+            searchBar.hide('slow');
+            searchBar.focusout(); //if no search string, close search bar
+        } //end else statement
+    }); //End search bar open function
 });
