c4530516a792cb0fc3780d31254346e3639eaaf7
lisath92 <lisa.hsu.419@gmail.com>
2016-08-18 19:11:34 -0700

    style single product page

--- themes/inhabitent-theme/single-product.php
+++ themes/inhabitent-theme/single-product.php
@@ -7,21 +7,29 @@
 
 get_header(); ?>
 
-  <div id="primary" class="content-area">
-    <main id="main" class="wrapper site-main" role="main">
+<div id="primary" class="content-area">
+  <main id="main" class="wrapper site-main" role="main">
 
     <?php while ( have_posts() ) : the_post(); ?>
+      <div class="single-product-image">
+       <?php if ( has_post_thumbnail() ) : ?>
+        <?php the_post_thumbnail( 'large' ); ?>
+      <?php endif; ?>
+    </div>
+    <section class="product-full-info">
+      <?php the_title( '<h1 class="product-name">', '</h1>' ); ?>
+      <?php echo '<h2 class="price">'.CFS()->get( 'price' ).'</h2>'; ?>
+      <?php the_content(); ?>
+      
+      <div class="social-media">
+        <button><i class="fa fa-facebook" aria-hidden="true"></i>Like</button>
+        <button><i class="fa fa-twitter" aria-hidden="true"></i>Tweet</button>
+        <button><i class="fa fa-pinterest"  aria-hidden="true"></i>Pin</button>
+      </div>
+    </section>
+  <?php endwhile; // End of the loop. ?>
 
-     <?php if ( has_post_thumbnail() ) : ?>
-      <?php the_post_thumbnail( 'large' ); ?>
-    <?php endif; ?>
-      <?php the_title( '<h2 class="product-name">', '</h1>' ); ?>
-    <?php echo CFS()->get( 'price' ); ?>
-    <?php the_content(); ?>
-
-    <?php endwhile; // End of the loop. ?>
-
-    </main><!-- #main -->
-  </div><!-- #primary -->
+</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
