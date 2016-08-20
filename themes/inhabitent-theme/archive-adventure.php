8963d1f819a5b431bf419e04232e36e14f87342c
lisath92 <lisa.hsu.419@gmail.com>
2016-08-19 20:02:12 -0700

    create adventure custom page and style adventure archive page

---
+++ themes/inhabitent-theme/archive-adventure.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @package Inhabitent_Theme
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="wrapper site-main" role="main">
+    <section class="adventures-grid">
+      <?php /* Start the Loop */ ?>
+      <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="adventure-img">
+            <?php if ( has_post_thumbnail() ) : ?>
+              <?php the_post_thumbnail( 'orignal' ); ?>
+              </div>
+              
+            <?php endif; ?>
+            <div class="adventure-info">
+
+            <?php the_title( sprintf( '<h2 class="adventure-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+            
+
+            <div class="read-more-box">
+              <p><a href="<?php the_permalink(); ?>" class="read-more">Read entry &#8594;</a></p>
+            </div>
+            </div>
+        </article>
+      <?php endwhile; ?>
+    </section>
+  </main><!-- #main -->
+</div><!-- #primary -->
+<?php get_footer(); ?>
--- themes/inhabitent-theme/js/main.js
+++ themes/inhabitent-theme/js/main.js
@@ -1,22 +1,23 @@
 $(function() {
 
-var searchButton = $('.search-submit');
-        var searchBar = $('.search-field');
+    var searchButton = $('.search-submit');
+    var searchBar = $('.search-field');
     //Opens search bar once search icon has been clicked
     $('body').on('click', function(event) {
         console.log(event.target);
 
         if ($(event.target).parents().hasClass('search-submit')) {
             event.preventDefault();
-            searchBar.show('slow');
-            searchBar.focus(); //check if button was clicked
-            searchButton.submit();
-        } else if (searchBar.val().length > 0) {//end first if statement
-            searchBar.focusout();
-            searchBar.show(); //check if there is entered search string
-        } else { //end second if statement
-            searchBar.hide('slow');
-            searchBar.focusout(); //if no search string, close search bar
-        } //end else statement
+            searchBar.toggle('slow');
+                searchBar.focus(); //check if button was clicked
+                searchButton.submit();
+            } else if ($(event.target).parents().hasClass('search-form') ||
+                searchBar.val().length > 0) { //end first if statement
+                searchBar.focusout();
+                searchBar.show(); //check if there is entered search string
+            } else {
+                searchBar.hide('slow');
+            }
+            
+        }); //if no search string, close search bar //end else statement
     }); //End search bar open function
-});
