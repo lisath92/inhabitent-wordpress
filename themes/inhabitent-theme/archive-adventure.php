bc94d824ec58312f39ef6c86960b63ea7d0b6af1
lisath92 <lisa.hsu.419@gmail.com>
2016-08-19 21:28:38 -0700

    style single adventure page

--- themes/inhabitent-theme/archive-adventure.php
+++ themes/inhabitent-theme/archive-adventure.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * The template for displaying adventure archive pages.
  *
  * @package Inhabitent_Theme
  */
---
+++ themes/inhabitent-theme/single-adventure.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package Inhabitent_Theme
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+      <div class="single-adventure-image">
+       <?php if ( has_post_thumbnail() ) : ?>
+        <?php the_post_thumbnail( 'full' ); ?>
+      <?php endif; ?>
+    </div>
+    <section class="wrapper adventure-full-info">
+      <?php the_title( '<h1 class="adventure-header">', '</h1>' ); ?>
+
+      <h3><?php inhabitent_posted_by(); ?></h3>
+      <?php the_content(); ?>
+      
+      <div class="social-media">
+        <button><i class="fa fa-facebook" aria-hidden="true"></i>Like</button>
+        <button><i class="fa fa-twitter" aria-hidden="true"></i>Tweet</button>
+        <button><i class="fa fa-pinterest"  aria-hidden="true"></i>Pin</button>
+      </div>
+    </section>
+  <?php endwhile; // End of the loop. ?>
+
+</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
